8bae26c149c144db99538f819c986fbbb54749b2
Albertinou <albertuciukas@gmail.com>
2022-06-01 14:02:23 +0300

    first commit

---
+++ src/Pages/Partners.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function partners() {
+  return <h1>Partners</h1>;
+}
+
+export default partners;
---
+++ src/Pages/Products.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function products() {
+  return <h1>Product list</h1>;
+}
+
+export default products;
