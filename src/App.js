b23e7001e50ebc493915d3fe2cc6820a58b971ec
Albertinou <albertuciukas@gmail.com>
2022-06-01 14:43:03 +0300

    added nav buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import Products from "./Pages/Products";
 import Partners from "./Pages/Partners";
@@ -8,6 +8,11 @@ import ErrorPage from "./Pages/ErrorPage";
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/products">Produktai</Link>
+        <Link to="/partnersgit">Partneriai</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/partners" element={<Partners />} />
